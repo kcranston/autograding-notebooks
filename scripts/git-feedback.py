3008a931da85591182de3a76ca14563501d05cef
Karen Cranston <karen.cranston@gmail.com>
2019-08-01 09:52:10 -0400

    fixing paths

--- scripts/git-feedback.py
+++ scripts/git-feedback.py
@@ -19,31 +19,36 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--roster', help='CSV roster file', default='roster.csv')
     parser.add_argument('--assignment', help='Name of assignment, e.g., hw1-rootfinding', required=True)
+    parser.add_argument('--repo_location',help='Directory containing cloned student repos', required=True)
     parser.add_argument('-n', '--dry-run', help='Print git statements but do not run', action='store_true')
     args = parser.parse_args()
 
     assignment = args.assignment
+    repo_location = args.repo_location
     roster = read_roster(args.roster)
     for identikey, (github_username,) in roster.iterrows():
         try:
-            (source,) = glob.glob(os.path.join('feedback', github_username, assignment, '*.html'))
+            (source,) = glob.glob(os.path.join('feedback', identikey, assignment, '*.html'))
         except ValueError: # Lack of feedback usually means student did not submit homework
-            print(f'No feedback for {github_username}')
+            print(f'No feedback for {identikey}')
             continue
-        destdir = os.path.join('submitted', github_username, assignment)
-        dest = os.path.join(destdir, 'feedback.html')
+        # local student repos named assignment-github_username
+        repo_name = "{}-{}".format(assignment,github_username)
+        destdir = os.path.join(repo_location, repo_name)
         if os.path.exists(destdir):
+            print("Copying feedback from {} to {}".format(source,destdir))
             shutil.copyfile(source, dest)
         else:
             print('Destination directory does not exist: {}'.format(destdir))
         git = ['git', '-C', destdir]
-        if (dry_run):
-            print("{} add {}".format(git,os.path.basename(dest)))
+        feedback_file = os.path.basename(source)
+        if (args.dry_run):
+            print("{} add {}".format(git,feedback_file))
             print("{} commit -m \"Add feedback\"".format(git))
             print("{} push".format(git))
         else:
             try:
-                subprocess.check_call(git + ['add', os.path.basename(dest)])
+                subprocess.check_call(git + ['add', feedback_file])
                 subprocess.check_call(git + ['commit', '-mAdd feedback'])
                 subprocess.check_call(git + ['push'])
             except subprocess.CalledProcessError:
