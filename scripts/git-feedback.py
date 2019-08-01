a2283cd90b82c415540b761b38c9ac28b5ac49d3
Karen Cranston <karen.cranston@gmail.com>
2019-08-01 08:53:21 -0400

    consistency with other scripts, remove email

--- scripts/git-feedback.py
+++ scripts/git-feedback.py
@@ -1,41 +1,50 @@
 #!/usr/bin/env python3
 # Script to push nbgrader html feedback reports to student repos for a given
 # assignment in a GitHub classroom course
-# Script provided by @jedbrown 
+# Script provided by @jedbrown
+# assumes that cwd is nbgrader course dir
 
 import glob
-import pandas
+import pandas as pd
 import os
 import shutil
 import subprocess
 
 def read_roster(filename):
-    return pandas.read_csv(filename, index_col=['User ID'], usecols=[1,5])
+    roster = pd.read_csv(args.roster, usecols=('identifier', 'github_username')).set_index('identifier')
+    return roster
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('--roster', help='CSV roster file', default='CSCI-3656-001-20190205.csv')
+    parser.add_argument('--roster', help='CSV roster file', default='roster.csv')
     parser.add_argument('--assignment', help='Name of assignment, e.g., hw1-rootfinding', required=True)
+    parser.add_argument('-n', '--dry-run', help='Print git statements but do not run', action='store_true')
     args = parser.parse_args()
-    roster = read_roster(args.roster)
+
     assignment = args.assignment
-    for username, address in roster['Email'].items():
+    roster = read_roster(args.roster)
+    for identikey, (github_username,) in roster.iterrows():
         try:
-            (source,) = glob.glob(os.path.join('feedback', username, assignment, '*.html'))
+            (source,) = glob.glob(os.path.join('feedback', github_username, assignment, '*.html'))
         except ValueError: # Lack of feedback usually means student did not submit homework
-            print(f'No feedback for {username}')
+            print(f'No feedback for {github_username}')
             continue
-        destdir = os.path.join('submitted', username, assignment)
+        destdir = os.path.join('submitted', github_username, assignment)
         dest = os.path.join(destdir, 'feedback.html')
         if os.path.exists(destdir):
             shutil.copyfile(source, dest)
         else:
             print('Destination directory does not exist: {}'.format(destdir))
         git = ['git', '-C', destdir]
-        try:
-            subprocess.check_call(git + ['add', os.path.basename(dest)])
-            subprocess.check_call(git + ['commit', '-mAdd feedback'])
-            subprocess.check_call(git + ['push'])
-        except subprocess.CalledProcessError:
-            print('Skipping {}'.format(destdir))
+        if (dry_run):
+            print("{} add {}".format(git,os.path.basename(dest)))
+            print("{} commit -m \"Add feedback\"".format(git))
+            print("{} push".format(git))
+        else:
+            try:
+                subprocess.check_call(git + ['add', os.path.basename(dest)])
+                subprocess.check_call(git + ['commit', '-mAdd feedback'])
+                subprocess.check_call(git + ['push'])
+            except subprocess.CalledProcessError:
+                print('Skipping {}'.format(destdir))
