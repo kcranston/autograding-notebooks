14b47305ef1e4ac929721ef4d5803861fce20b67
Karen Cranston <karen.cranston@gmail.com>
2019-08-13 15:54:12 -0400

    clone into separate directory, not nbgrader dir

--- scripts/clone-all.py
+++ scripts/clone-all.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 # Script to clone all student repos from a given GitHub classroom course
-# into a directory called `submitted` in the correct format for nbgrader
+# and then copy over the assignment files into the nbgrader `submitted` dir
 # Modifed from script received from @jedbrown to remove specific course and
-# repo references
-# assumes that cwd is nbgrader course dir
+# repo references and to clone into separate directory (we are keeping the
+# nbgrader repo under version control, so avoiding repos-within-repos)
+# Assumes that cwd is nbgrader course dir
 
 import pandas as pd
 import os
+import glob
+import shutil
+import subprocess
 
 def mkdir_p(path):
     dirname = os.path.dirname(path)
@@ -18,22 +22,30 @@ def mkdir_p(path):
     except FileExistsError:
         pass
 
-def clone_repo(url, dest, args):
-    import subprocess
-    if os.path.isdir(dest):
+def clone_repo(slug, username, args):
+    clonedir = args.clonedir # directory containing git repos
+    destdir = os.path.join(clonedir,slug) # path to repo
+
+    if os.path.isdir(destdir):
+        # if the repo already exists, pull (unless skip_existing)
         if args.skip_existing:
             return
-        cmd = ['git', '-C', dest, 'pull']
+        cmd = ['git', '-C', destdir, 'pull']
         cmdargs = []
     else:
-        mkdir_p(os.path.dirname(dest))
-        cmd = ['git', 'clone']
-        cmdargs = [url, dest]
+        classroom = args.classroom
+        assignment = args.assignment
+        # url format is git@github.com:classroom/assignment-github_username.git
+        url = "git@github.com:{}/{}.git".format(classroom, slug)
+        # repo does not already exist; clone
+        cmd = ['git', '-C', clonedir, 'clone']
+        cmdargs = [url]
     if args.dry_run:
         cmd.append('--dry-run')
         print(cmd + cmdargs)
         return
     return subprocess.call(cmd + cmdargs)
+
 
 if __name__ == '__main__':
     # argument parsing
@@ -42,6 +54,7 @@ if __name__ == '__main__':
     parser.add_argument('--roster', help='CSV file from which to read roster', default='classroom_roster.csv')
     parser.add_argument('--assignment', help='Assignment name, e.g., "2019-01-31-stability" or "hw1-rootfinding"', required=True)
     parser.add_argument('--classroom', help='GitHub Classroom name', required=True)
+    parser.add_argument('--clonedir', help='Destination directory', default='../cloned-repos')
     parser.add_argument('-n', '--dry-run', help='List repositories that would be cloned', action='store_true')
     parser.add_argument('--skip-existing', help='Skip attempt to update repositories that have already been cloned', action='store_true')
     args = parser.parse_args()
@@ -49,15 +62,29 @@ if __name__ == '__main__':
     # roster associates github usernames and student identifiers
     roster = pd.read_csv(args.roster, usecols=('identifier', 'github_username')).set_index('identifier')
     classroom = args.classroom
+    assignment = args.assignment
 
-    # url format is git@github.com:classroom/assignment-github_username.git
-    url_slug = "git@github.com:{}/{}".format(classroom, args.assignment)
     missing = []
+    clonedir = args.clonedir
+    mkdir_p(clonedir)
+
     for identikey, (github_username,) in roster.iterrows():
-        if clone_repo(url='{}-{}.git'.format(url_slug,github_username),
-                      dest='submitted/{}/{}'.format(identikey, args.assignment),
-                      args=args):
+        slug = "{}-{}".format(assignment,github_username)
+        if clone_repo(
+            slug = slug,
+            username = github_username,
+            args=args):
+        #if clone_repo(url='{}-{}.git'.format(url_slug,github_username),
+                      #dest='submitted/{}/{}'.format(identikey, assignment),
+                      #args=args):
             missing.append((identikey, github_username))
+        else:
+            files = glob.glob(os.path.join(clonedir,slug,'*.ipynb'))
+            dest = os.path.join('submitted',github_username,assignment)
+            mkdir_p(dest)
+            for f in files:
+                print("copying {} to {}".format(f,dest))
+                shutil.copy(f, dest)
     if len(missing)==0:
         print('All successful; no missing repos')
     else:
