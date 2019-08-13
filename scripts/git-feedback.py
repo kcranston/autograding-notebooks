d1f38cc65d12d4925c7334fa2be711053f2c3b33
Karen Cranston <karen.cranston@gmail.com>
2019-08-13 19:45:05 -0400

    changed to work with repos outside of nbgrader dir

--- scripts/git-feedback.py
+++ scripts/git-feedback.py
@@ -1,14 +1,38 @@
 #!/usr/bin/env python3
-# Script to push nbgrader html feedback reports to student repos for a given
-# assignment in a GitHub classroom course
-# Script provided by @jedbrown
-# assumes that cwd is nbgrader course dir
+# Script to copy nbgrader html feedback reports to feedback.html in student
+# repos and push repos to github
+# Based on script provided by @jedbrown
+# Assumes that cwd is nbgrader course dir
 
 import glob
 import pandas as pd
 import os
 import shutil
 import subprocess
+
+def run_cmd(cmd,cmdargs,dry_run):
+    if (dry_run):
+        cmdargs.insert(1,'--dry-run')
+        print(cmd+cmdargs)
+    subprocess.check_output(cmd + cmdargs)
+
+def do_git_things(destdir, dest, dry_run):
+    gitcmd = ['git', '-C', destdir]
+
+    try:
+        # add
+        cmdargs = ['add', os.path.basename(dest)]
+        run_cmd(gitcmd,cmdargs,dry_run)
+        # commit
+        cmdargs = ['commit', '-mAdd feedback.html']
+        run_cmd(gitcmd,cmdargs,dry_run)
+        # push
+        cmdargs = ['push']
+        run_cmd(gitcmd,cmdargs,dry_run)
+    except subprocess.CalledProcessError as e:
+        print(e.output)
+        print('Skipping {}'.format(destdir))
+
 
 def read_roster(filename):
     roster = pd.read_csv(args.roster, usecols=('identifier', 'github_username')).set_index('identifier')
@@ -19,36 +43,27 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--roster', help='CSV roster file', default='roster.csv')
     parser.add_argument('--assignment', help='Name of assignment, e.g., hw1-rootfinding', required=True)
+    parser.add_argument('--clonedir', help='Destination directory', default='../cloned-repos')
     parser.add_argument('-n', '--dry-run', help='Print git statements but do not run', action='store_true')
     args = parser.parse_args()
 
     assignment = args.assignment
+    clonedir = args.clonedir
 
     roster = read_roster(args.roster)
     for identikey, (github_username,) in roster.iterrows():
         try:
-            (source,) = glob.glob(os.path.join('feedback', identikey, assignment, '*.html'))
+            #print("Looking in: {}".format(os.path.join('feedback', identikey, assignment)))
+            (source,) = glob.glob(os.path.join('feedback', identikey, assignment, '*.html'),recursive=True)
         except ValueError: # Lack of feedback usually means student did not submit homework
-            print(f'No feedback for {identikey}')
+            print("No feedback found for {}".format(identikey))
             continue
-
-        destdir = os.path.join('submitted', github_username, assignment)
+        slug = "{}-{}".format(assignment,github_username)
+        destdir = os.path.join(clonedir, slug)
         dest = os.path.join(destdir, 'feedback.html')
         if os.path.exists(destdir):
-            print("Copying feedback from {} to {}".format(source,destdir))
+            print("Copying feedback from {} to {}".format(source,dest))
             shutil.copyfile(source, dest)
         else:
             print('Destination directory does not exist: {}'.format(destdir))
-        git = ['git', '-C', destdir]
-
-        if (args.dry_run):
-            print("{} add {}".format(git,os.path.basename(dest)))
-            print("{} commit -m \"Add feedback\"".format(git))
-            print("{} push".format(git))
-        else:
-            try:
-                subprocess.check_call(git + ['add', os.path.basename(dest)])
-                subprocess.check_call(git + ['commit', '-mAdd feedback'])
-                subprocess.check_call(git + ['push'])
-            except subprocess.CalledProcessError:
-                print('Skipping {}'.format(destdir))
+        do_git_things(destdir, dest, args.dry_run)
